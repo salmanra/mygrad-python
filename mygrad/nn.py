31ea29080066eb3d40f549b1e0dd171d4994f9d6
salmanra <salmanr@umich.edu>
2023-07-17 21:30:14 -0400

    we now have an FC layer... what does it feel like against micrograd's FC layer?

--- mygrad/nn.py
+++ mygrad/nn.py
@@ -1,10 +1,27 @@
 import random
+import numpy as np
+
 from mygrad.engine import Tensor
+
 
 ## want to build up to a MLP using Tensors as defined in engine.py
 class Module:
-    def zero_grad():
-        print('implemnt zerograd')\
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self):
+        return []
+    
+class FullyConnectedLayer(Module):
+    # needs an in-dim, an out-dim, and an act function
+    def __init__(self, indim, outdim, act='relu') -> None:
+        # He initialization, the goat
+        self.W = Tensor(np.random.default_rng().normal(0, 2/(indim*outdim), size=indim*outdim).reshape(outdim, indim))
+        self.b = Tensor(np.zeros(shape=(outdim)))
+        # there's got to be a better way to make the activation function then just "look it up"
+        self.act = act
+
+    def __call__(self, other):
+        return ((self.W @ other) + self.b).relu()
         
-    def parameters():
-        print('implement parameters')
